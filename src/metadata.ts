f08ee7d433af49f0f99794e92b85c90479a55a0d
szikszail <sziklaszlo@gmail.com>
2021-01-28 00:21:49 +0100

    Fixed logs

--- src/metadata.ts
+++ src/metadata.ts
@@ -15,7 +15,7 @@ export const readMetadata = (file: string): Promise<LockMetadata> => {
     return new Promise<LockMetadata>((resolve, reject): void => {
         logger("readMetadata file:%s", file);
         readFile(file, { encoding: "utf8" }, (err: Error, data: Buffer) => {
-            logger("readMetadata err:%O", err)
+            logger("readMetadata err:%s", err)
             logger("readMetadata data:%O", data);
             if (err) {
                 reject(err);
@@ -25,7 +25,7 @@ export const readMetadata = (file: string): Promise<LockMetadata> => {
                     logger("readMetadata parsed:%O", parsed);
                     resolve(parsed);
                 } catch (e) {
-                    logger("readMetadata not JSON:%O", e);
+                    logger("readMetadata not JSON:%s", e);
                     resolve(null);
                 }
             }
@@ -41,18 +41,19 @@ export const readMetadata = (file: string): Promise<LockMetadata> => {
  */
 export const saveMetadata = (file: string, lockTime: number): Promise<void> => {
     return new Promise((resolve, reject) => {
-        logger("saveMetadata", file, lockTime);
+        logger("saveMetadata file:%s", file)
+        logger("saveMetadata lockTime:%d", lockTime);
         const metadata: LockMetadata = {
             pID: process.pid,
             lockTime
         };
-        logger("saveMetadata", metadata);
+        logger("saveMetadata metadata:%o", metadata);
         writeFile(
             file,
             JSON.stringify(metadata),
             { encoding: "utf8" },
             (err: Error): void => {
-                logger("saveMetadata", "done", err);
+                logger("saveMetadata done err:%o", err);
                 if (err) {
                     reject(err);
                 } else {
--- src/unlock.ts
+++ src/unlock.ts
@@ -5,12 +5,12 @@ import { LockMetadata, readMetadata } from "./metadata";
 export type UnlockFunction = (file?: string) => Promise<void>;
 
 export const unlock: UnlockFunction = async (file: string): Promise<void> => {
-    logger("unlock", file);
+    logger("unlock file:%s", file);
 
     const lockFile: string = `${file}.lock`;
 
     if (!existsSync(lockFile)) {
-        logger("unlock", "lock file does not exist");
+        logger("unlock lock file does not exist");
         return;
     }
 
@@ -19,7 +19,7 @@ export const unlock: UnlockFunction = async (file: string): Promise<void> => {
         try {
             unlinkSync(lockFile);
         } catch (e) {
-            logger("unlock", "error during deleting lock file", e);
+            logger("unlock error during deleting lock file:%s", e);
         }
     }
 };
