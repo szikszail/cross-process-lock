c0f194e69d68844986a64e41fbfa5f8940e5f2fb
szikszail <sziklaszlo@gmail.com>
2026-02-07 19:29:23 +0100

    2.2.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,5 +11,5 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
-  globalIgnores(["tests/scripts"]),
+  globalIgnores(["tests/scripts", "dist", "node_modules"]),
 ]);
